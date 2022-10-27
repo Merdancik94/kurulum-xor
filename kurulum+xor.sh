4a16c80a8da1a6bed803ab9c3046c6d425fc8ed9
Merdancik94 <97890006+Merdancik94@users.noreply.github.com>
2022-10-27 19:36:23 +0500

    Update kurulum+xor.sh

--- kurulum+xor.sh
+++ kurulum+xor.sh
@@ -37,7 +37,7 @@ install_openvpn() {
 	cd /var/www/html
 	git clone https://github.com/furlongm/openvpn-monitor.git
         echo "management 127.0.0.1 5555" >> /etc/openvpn/server/server.conf
-        echo "scramble obfuscate EBkBYm31pYQVnffzfHXSE0Da+EgoQfzC" >> /etc/openvpn/server/server.conf
+        echo "scramble obfuscate DdV4yAhJOagzdSZDXLQLJgOGqM6PlJYs" >> /etc/openvpn/server/server.conf
 	service openvpn restart
 	service openvpn-server@server restart
 
