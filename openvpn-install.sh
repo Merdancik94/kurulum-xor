54884d7bec126f5daa157f5f267ee58a63a7bec5
Merdancik94 <97890006+Merdancik94@users.noreply.github.com>
2022-12-11 00:44:11 +0500

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -422,7 +422,7 @@ persist-key
 persist-tun
 remote-cert-tls server
 auth SHA512
-scramble obfuscate ks5Do8YIqYFPgDtb/qBkQ19ntzkcxiDh
+scramble obfuscate phEWQ7d3JsAL9pUa/3lvfUQX6TReFhVy
 cipher AES-256-CBC
 ignore-unknown-option block-outside-dns
 block-outside-dns
